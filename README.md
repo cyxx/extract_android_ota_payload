5f1b4f3ff7711f4a518b7dcc6f680c0ce6d707e1
Gregory Montoir <cyx@users.sourceforge.net>
2018-06-26 15:47:32 +0800

    Add initial version of payload.bin extraction script

---
+++ README.md
@@ -0,0 +1,26 @@
+# extract_android_ota_payload.py
+
+Extract Android firmware images from an OTA payload.bin file.
+
+With the introduction of the A/B system update, the OTA file format changed.
+This tool allows to extract and decompress the firmware images packed using the 'brillo' toolset.
+
+## Usage
+
+```
+$ extract_android_ota_payload.py <payload.bin> [target_dir]
+  <payload.bin> : file extracted from the OTA zip file or the OTA zip file
+  <target_dir>  : output directory for the extracted file
+```
+
+## Example
+
+```
+$ python extract_android_ota_payload.py marlin-ota-opm4.171019.021.d1-fd6998a5.zip /tmp/
+Extracting 'boot.img'
+Extracting 'system.img'
+Extracting 'vendor.img'
+...
+Extracting 'modem.img'
+```
+
---
+++ extract_android_ota_payload.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import hashlib
+import os
+import os.path
+import shutil
+import subprocess
+import sys
+import zipfile
+
+# from https://android.googlesource.com/platform/system/update_engine/scripts/
+import update_payload
+
+PROGRAMS = [ 'bzcat', 'xzcat' ]
+
+def decompress_payload(command, data, size, hash):
+  p = subprocess.Popen([command, '-'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+  r = p.communicate(data)[0]
+  if len(r) != size:
+    print("Unexpected size %d %d" % (len(r), size))
+  return r
+
+def parse_payload(payload_f, partition, out_f):
+  BLOCK_SIZE = 4096
+  for operation in partition.operations:
+    e = operation.dst_extents[0]
+    data = payload_f.ReadDataBlob(operation.data_offset, operation.data_length)
+    out_f.seek(e.start_block * BLOCK_SIZE)
+    if operation.type == update_payload.common.OpType.REPLACE:
+      out_f.write(data)
+    elif operation.type == update_payload.common.OpType.REPLACE_XZ:
+      r = decompress_payload('xzcat', data, e.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
+      out_f.write(r)
+    elif operation.type == update_payload.common.OpType.REPLACE_BZ:
+      r = decompress_payload('bzcat', data, e.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
+      out_f.write(r)
+    else:
+      raise update_payload.error.PayloadError('Unknown operation type (%d)' % operation.type)
+
+def main(argv):
+  try:
+    filename = argv[1]
+  except:
+    print('Usage: %s payload.bin [output_dir]' % argv[0])
+    sys.exit()
+
+  try:
+    output_dir = argv[2]
+  except IndexError:
+    output_dir = os.getcwd()
+
+  if filename.endswith('.zip'):
+    print("Extracting 'payload.bin' from OTA file...")
+    ota_zf = zipfile.ZipFile(filename)
+    payload_file = open(ota_zf.extract('payload.bin', output_dir))
+  else:
+    payload_file = file(filename)
+  
+  payload = update_payload.Payload(payload_file)
+  payload.Init()
+  payload.Describe()
+
+  if payload.header.version != update_payload.common.BRILLO_MAJOR_PAYLOAD_VERSION:
+    printf("Unsupported payload version (%d)" % payload.header.version)
+  else:
+    for p in payload.manifest.partitions:
+      name = p.partition_name + '.img'
+      print("Extracting '%s'" % name)
+      out_f = open(os.path.join(output_dir, name), 'w')
+      parse_payload(payload, p, out_f)
+
+if __name__ == '__main__':
+  main(sys.argv)
