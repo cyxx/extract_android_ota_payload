d4882134c1912347a7ae6b9acb246f5c7c8ae3d4
Gregory Montoir <cyx@users.sourceforge.net>
2018-08-23 16:20:02 +0900

    Remove unnecessary code
    This keeps the upstream generated protobuf python code.

--- extract_android_ota_payload.py
+++ extract_android_ota_payload.py
@@ -4,14 +4,75 @@ import hashlib
 import os
 import os.path
 import shutil
+import struct
 import subprocess
 import sys
 import zipfile
 
 # from https://android.googlesource.com/platform/system/update_engine/scripts/
-import update_payload
+import update_metadata_pb2
 
 PROGRAMS = [ 'bzcat', 'xzcat' ]
+
+BRILLO_MAJOR_PAYLOAD_VERSION = 2
+
+class PayloadError(Exception):
+  pass
+
+class Payload(object):
+  class _PayloadHeader(object):
+    _MAGIC = 'CrAU'
+
+    def __init__(self):
+      self.version = None
+      self.manifest_len = None
+      self.metadata_signature_len = None
+      self.size = None
+
+    def ReadFromPayload(self, payload_file):
+      magic = payload_file.read(4)
+      if magic != self._MAGIC:
+        raise PayloadError('Invalid payload magic: %s' % magic)
+      self.version = struct.unpack('>Q', payload_file.read(8))[0]
+      self.manifest_len = struct.unpack('>Q', payload_file.read(8))[0]
+      self.size = 20
+      self.metadata_signature_len = 0
+      if self.version != BRILLO_MAJOR_PAYLOAD_VERSION:
+        raise PayloadError('Unsupported payload version (%d)' % self.version)
+      self.size += 4
+      self.metadata_signature_len = struct.unpack('>I', payload_file.read(4))[0]
+
+  def __init__(self, payload_file):
+    self.payload_file = payload_file
+    self.header = None
+    self.manifest = None
+    self.data_offset = None
+    self.metadata_signature = None
+    self.metadata_size = None
+
+  def _ReadManifest(self):
+    return self.payload_file.read(self.header.manifest_len)
+
+  def _ReadMetadataSignature(self):
+    self.payload_file.seek(self.header.size + self.header.manifest_len)
+    return self.payload_file.read(self.header.metadata_signature_len);
+
+  def ReadDataBlob(self, offset, length):
+    self.payload_file.seek(self.data_offset + offset)
+    return self.payload_file.read(length)
+
+  def Init(self):
+    self.header = self._PayloadHeader()
+    self.header.ReadFromPayload(self.payload_file)
+    manifest_raw = self._ReadManifest()
+    self.manifest = update_metadata_pb2.DeltaArchiveManifest()
+    self.manifest.ParseFromString(manifest_raw)
+    metadata_signature_raw = self._ReadMetadataSignature()
+    if metadata_signature_raw:
+      self.metadata_signature = update_metadata_pb2.Signatures()
+      self.metadata_signature.ParseFromString(metadata_signature_raw)
+    self.metadata_size = self.header.size + self.header.manifest_len
+    self.data_offset = self.metadata_size + self.header.metadata_signature_len
 
 def decompress_payload(command, data, size, hash):
   p = subprocess.Popen([command, '-'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
@@ -28,16 +89,16 @@ def parse_payload(payload_f, partition, out_f):
     e = operation.dst_extents[0]
     data = payload_f.ReadDataBlob(operation.data_offset, operation.data_length)
     out_f.seek(e.start_block * BLOCK_SIZE)
-    if operation.type == update_payload.common.OpType.REPLACE:
+    if operation.type == update_metadata_pb2.InstallOperation.REPLACE:
       out_f.write(data)
-    elif operation.type == update_payload.common.OpType.REPLACE_XZ:
+    elif operation.type == update_metadata_pb2.InstallOperation.REPLACE_XZ:
       r = decompress_payload('xzcat', data, e.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
       out_f.write(r)
-    elif operation.type == update_payload.common.OpType.REPLACE_BZ:
+    elif operation.type == update_metadata_pb2.InstallOperation.REPLACE_BZ:
       r = decompress_payload('bzcat', data, e.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
       out_f.write(r)
     else:
-      raise update_payload.error.PayloadError('Unhandled operation type (%d)' % operation.type)
+      raise PayloadError('Unhandled operation type (%d)' % operation.type)
 
 def main(argv):
   try:
@@ -58,24 +119,20 @@ def main(argv):
   else:
     payload_file = file(filename)
 
-  payload = update_payload.Payload(payload_file)
+  payload = Payload(payload_file)
   payload.Init()
-  payload.Describe()
 
-  if payload.header.version != update_payload.common.BRILLO_MAJOR_PAYLOAD_VERSION:
-    print('Unsupported payload version (%d)' % payload.header.version)
-  else:
-    for p in payload.manifest.partitions:
-      name = p.partition_name + '.img'
-      print("Extracting '%s'" % name)
-      fname = os.path.join(output_dir, name)
-      out_f = open(fname, 'w')
-      try:
-        parse_payload(payload, p, out_f)
-      except update_payload.error.PayloadError as e:
-        print('Failed: %s' % e)
-        out_f.close()
-        os.unlink(fname)
+  for p in payload.manifest.partitions:
+    name = p.partition_name + '.img'
+    print("Extracting '%s'" % name)
+    fname = os.path.join(output_dir, name)
+    out_f = open(fname, 'w')
+    try:
+      parse_payload(payload, p, out_f)
+    except update_payload.error.PayloadError as e:
+      print('Failed: %s' % e)
+      out_f.close()
+      os.unlink(fname)
 
 if __name__ == '__main__':
   main(sys.argv)
--- update_payload/error.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-"""Payload handling errors."""
-
-
-class PayloadError(Exception):
-  """An update payload general processing error."""
